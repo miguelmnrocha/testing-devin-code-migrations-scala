4d16aa2e27b689c99214c73bcf0c78e4062f2a73
Devin <devin@example.com>
2024-04-19 10:19:24 +0000

    Remove testing-devin-code-migrations-scala folders from main and test directories

--- src/test/scala/testing-devin-code-migrations-scala/HelloSpec.scala
+++
@@ -1,7 +0,0 @@
-package testing_devin_code_migrations_scala
-
-class HelloSpec extends munit.FunSuite {
-  test("say hello") {
-    assertEquals(testing_devin_code_migrations_scala.Hello.greeting, "hello")
-  }
-}
