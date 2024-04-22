6a7a257721b026de1574b3b49960e18d47382a55
Ubuntu <ubuntu@ip-10-240-149-191.us-west-2.compute.internal>
2024-04-22 19:18:05 +0000

    Move SparkOpInstance1Test to new submodule structure

--- src/test/scala/subdomains/domainA/sparkops/SparkOpInstance1Test.scala
+++ src/test/scala/subdomains/domainA/sparkops/SparkOpInstance1Test.scala
@@ -5,6 +5,4 @@ class SparkOpInstance1Test extends AnyFunSuite {
   test("SparkOpInstance1: name should follow naming conventions") {
     assert(SparkOpInstance1.name.matches("^dataset/.+-.+$") || SparkOpInstance1.name.matches("^nu-br/dataset/.+-.+$"))
   }
-
-  // Additional tests for inputs, query, metadata, and runConfigurations can be added here
 }
